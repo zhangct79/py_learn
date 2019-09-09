7120da080f8e19fe9275effc62159e4a5ffc2305
zhangct <zhangct@siyuanren.com>
2019-09-09 23:41:47 +0800

    高级特征

---
+++ 03adv-feature/iteration.py
@@ -0,0 +1,9 @@
+#迭代#
+
+from collections import Iterable
+
+print(isinstance('abc', Iterable))
+print(isinstance([1,2,3], Iterable))
+print(isinstance(123, Iterable))
+
+
---
+++ 03adv-feature/slices.py
@@ -0,0 +1,11 @@
+#切片#
+
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+
+print(L[:3])
+print(L[0:3])
+
+print(L[-3:])
+print(L[-3:-1])
+
+print(L[1:2])
