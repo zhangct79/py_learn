7a1e1418ab5662884f94367843b5d64c41b53760
zhangchuntao1 <zhangchuntao1@open.com.cn>
2020-05-11 00:29:52 +0800

    set

--- 01basic/set.py
+++ 01basic/set.py
@@ -7,3 +7,10 @@ print(s)
 
 s.remove(2)
 print(s)
+
+jenkins = {1,2,4,5}
+mysql = {1,2,3}
+
+# 增加4,5，删除3
+print(jenkins - mysql)
+print(mysql - jenkins)
--- 14email/01smtp.py
+++ 14email/01smtp.py
@@ -12,7 +12,7 @@ def _format_addr(s):
 
 from_addr = '31872379@qq.com'
 password = 'Qwj8586dircls'
-to_addr = 'zhangct@siyuanren.com'
+to_addr = '31872379@qq.com'
 smtp_server = 'smtp.qq.com'
 
 # 邮件对象:
@@ -24,22 +24,22 @@ msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
 # 邮件正文是MIMEText:
 msg.attach(MIMEText('send with file...', 'plain', 'utf-8'))
 
-# 添加附件就是加上一个MIMEBase，从本地读取一个图片:
-with open('attachment//test.jpg', 'rb') as f:
-    # 设置附件的MIME和文件名，这里是png类型:
-    mime = MIMEBase('image', 'png', filename='test.png')
-    # 加上必要的头信息:
-    mime.add_header('Content-Disposition', 'attachment', filename='test.png')
-    mime.add_header('Content-ID', '<0>')
-    mime.add_header('X-Attachment-Id', '0')
-    # 把附件的内容读进来:
-    mime.set_payload(f.read())
-    # 用Base64编码:
-    encoders.encode_base64(mime)
-    # 添加到MIMEMultipart:
-    msg.attach(mime)
+# # 添加附件就是加上一个MIMEBase，从本地读取一个图片:
+# with open('attachment//test.jpg', 'rb') as f:
+#     # 设置附件的MIME和文件名，这里是png类型:
+#     mime = MIMEBase('image', 'png', filename='test.png')
+#     # 加上必要的头信息:
+#     mime.add_header('Content-Disposition', 'attachment', filename='test.png')
+#     mime.add_header('Content-ID', '<0>')
+#     mime.add_header('X-Attachment-Id', '0')
+#     # 把附件的内容读进来:
+#     mime.set_payload(f.read())
+#     # 用Base64编码:
+#     encoders.encode_base64(mime)
+#     # 添加到MIMEMultipart:
+#     msg.attach(mime)
 
-server = smtplib.SMTP(smtp_server, 465)
+server = smtplib.SMTP(smtp_server, 587)
 server.set_debuglevel(1)
 server.login(from_addr, password)
 server.sendmail(from_addr, [to_addr], msg.as_string())
